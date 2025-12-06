e55256bcc4d18fcdd1b09074e7b697c3062c2432
Polina Sliusareva <ghileors@gmail.com>
2025-12-06 22:00:50 +0200

    config:  add a type declaration for *.svg?react

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
 /// <reference types="vitest/config" />
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
-import { playwright } from '@vitest/browser-playwright';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), svgr()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './app'),
