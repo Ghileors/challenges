0c3343102505bf4003f5e15b426a35b4b6d98c5e
Polina Sliusareva <ghileors@gmail.com>
2025-12-08 14:44:00 +0200

    feat: implement mocks for avatars

--- app/features/change-avatar-background/mocks.ts
+++ app/features/change-avatar-background/mocks.ts
@@ -1,3 +1,13 @@
+import AliceImage from '@/assets/images/avatars/alice.jpg';
+import AmberImage from '@/assets/images/avatars/amber.jpg';
+import CatherineImage from '@/assets/images/avatars/catherine.jpg';
+import EvelynImage from '@/assets/images/avatars/evelyn.jpg';
+import GabrielleImage from '@/assets/images/avatars/gabrielle.jpg';
+import JenniferImage from '@/assets/images/avatars/jennifer.jpg';
+import LaurenImage from '@/assets/images/avatars/lauren.jpg';
+import SamanthaImage from '@/assets/images/avatars/samantha.jpg';
+import SylvesterImage from '@/assets/images/avatars/sylvester.jpg';
+import TimImage from '@/assets/images/avatars/tim.jpg';
 import BgImage from '@/assets/images/backgrounds/bg-0.jpg';
 import BgImage1 from '@/assets/images/backgrounds/bg-1.jpg';
 
@@ -9,5 +19,58 @@ export const backgroundImagesMock = [
   {
     id: 'image1',
     image: BgImage1,
+  },
+];
+
+export const avatarsMock = [
+  {
+    id: 'd75e09d0-8a2f-4a32-bec0-29c0f9ea3d6f',
+    image: AliceImage,
+    name: 'Alice',
+  },
+  {
+    id: 'ec87cf4e-8a4e-4df2-a4a7-7e1aa2f28513',
+    image: AmberImage,
+    name: 'Amber',
+  },
+  {
+    id: '2de618dc-bb0d-45c1-a7b8-d401e2d2e565',
+    image: CatherineImage,
+    name: 'Catherine',
+  },
+  {
+    id: '77e0b1f6-ef2d-4f25-91b5-0bb3150dffb1',
+    image: EvelynImage,
+    name: 'Evelyn',
+  },
+  {
+    id: '9a3a764f-4c36-42ac-8f46-c1c8eacaf44c',
+    image: GabrielleImage,
+    name: 'Gabrielle',
+  },
+  {
+    id: '39db2c2c-4081-46e3-8582-5939c2b2cc8c',
+    image: JenniferImage,
+    name: 'Jennifer',
+  },
+  {
+    id: '16472c3e-94a3-4990-b358-16c4cbb01bd1',
+    image: LaurenImage,
+    name: 'Lauren',
+  },
+  {
+    id: 'f902bfa1-1501-4e76-a45f-daa0612e1822',
+    image: SamanthaImage,
+    name: 'Samantha',
+  },
+  {
+    id: '3b7e5d8e-b693-4041-a6b1-d83737c8db26',
+    image: SylvesterImage,
+    name: 'Sylvester',
+  },
+  {
+    id: '8d0c1c34-8801-4cc6-b5b0-0e05a9c2f118',
+    image: TimImage,
+    name: 'Tim',
   },
 ];
