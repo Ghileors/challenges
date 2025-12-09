583ed8b02813bc99dc98c072fd28241f48470941
Polina Sliusareva <ghileors@gmail.com>
2025-12-09 15:32:10 +0200

    refactor: delete extra routes

--- app/lib/constants.ts
+++
@@ -1,4 +0,0 @@
-export const routesMap = [
-  { url: 'about', pagePath: 'routes/about.tsx' },
-  { url: 'avatar', pagePath: 'routes/avatar.tsx' },
-];
--- app/routes/layout.tsx
+++ app/routes/layout.tsx
@@ -1,6 +1,5 @@
 import ArrowIcon from '@/assets/icons/arrow.svg?react';
 import CloseIcon from '@/assets/icons/close.svg?react';
-import { routesMap } from '@/lib/constants';
 import { cn } from '@/lib/utils';
 import { Link, Outlet } from 'react-router';
 
@@ -15,21 +14,30 @@ export default function Layout() {
         {/* TODO: add condition rendering for icons */}
         <ArrowIcon />
         <div className="flex gap-1.5">
-          {routesMap.map(({ url }) => (
-            <Link
-              key={url}
-              to={`/${url}`}
-              className={cn(
-                'flex h-[42px] w-fit items-center justify-center gap-[6px] rounded-full pt-[16px] pr-[16px] pb-[14px] pl-[16px]',
-                'border border-[hsla(210,18%,96%,1)] backdrop-blur-[15px]',
-                'focus:border-[hsla(208,22%,83%,1)] focus:text-black'
-              )}
-            >
-              <p className="align-bottom font-[Italian_Plate_No2_Expanded] text-[12px] leading-[100%] font-semibold tracking-[0] text-[hsla(208,6%,59%,1)] first-letter:uppercase">
-                {url}
-              </p>
-            </Link>
-          ))}
+          <Link
+            to="avatar"
+            className={cn(
+              'flex h-[42px] w-fit items-center justify-center gap-[6px] rounded-full pt-[16px] pr-[16px] pb-[14px] pl-[16px]',
+              'border border-[hsla(210,18%,96%,1)] backdrop-blur-[15px]',
+              'focus:border-[hsla(208,22%,83%,1)] focus:text-black'
+            )}
+          >
+            <p className="align-bottom font-[Italian_Plate_No2_Expanded] text-[12px] leading-[100%] font-semibold tracking-[0] text-[hsla(208,6%,59%,1)] first-letter:uppercase">
+              Avatar
+            </p>
+          </Link>
+          <Link
+            to="/"
+            className={cn(
+              'flex h-[42px] w-fit items-center justify-center gap-[6px] rounded-full pt-[16px] pr-[16px] pb-[14px] pl-[16px]',
+              'border border-[hsla(210,18%,96%,1)] backdrop-blur-[15px]',
+              'focus:border-[hsla(208,22%,83%,1)] focus:text-black'
+            )}
+          >
+            <p className="align-bottom font-[Italian_Plate_No2_Expanded] text-[12px] leading-[100%] font-semibold tracking-[0] text-[hsla(208,6%,59%,1)] first-letter:uppercase">
+              About
+            </p>
+          </Link>
         </div>
 
         <CloseIcon />
