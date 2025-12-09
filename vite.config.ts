56a3420ceb0ab4cf36c561f224543e4ff0e323c9
Polina Sliusareva <ghileors@gmail.com>
2025-12-09 11:43:54 +0200

    chore: add vercel plugin

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,14 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
+import vercel from 'vite-plugin-vercel';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), svgr()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), svgr(), vercel()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './app'),
