385ebfddb4c06ad62ef29453a92d47b46f96939d
Polina Sliusareva <ghileors@gmail.com>
2025-12-09 12:16:06 +0200

    fix: delete vercel plugin

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,13 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
-import vercel from 'vite-plugin-vercel';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), svgr(), vercel()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), svgr()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './app'),
