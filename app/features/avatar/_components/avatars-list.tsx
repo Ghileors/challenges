24876e3aea1ee317e0c53abbcd59a48683f299e9
Polina Sliusareva <ghileors@gmail.com>
2025-12-11 09:28:50 +0200

    feat: implement filters

--- app/features/avatar/_components/avatars-list.tsx
+++ app/features/avatar/_components/avatars-list.tsx
@@ -1,12 +1,11 @@
 import { Skeleton } from '@/components/ui/skeleton';
-import { useNavigate, useSearchParams } from 'react-router';
+import { useSearchParams } from 'react-router';
 
 import { useGetAvatars } from '../queries';
 import { AvatarPreview } from './avatar-preview';
 
 export function AvatarsList() {
-  const navigate = useNavigate();
-  const [searchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
   const { data, isLoading } = useGetAvatars();
 
   const id = searchParams.get('id');
@@ -24,15 +23,22 @@ export function AvatarsList() {
     );
   }
 
+  const handleSelect = (id: string) => {
+    setSearchParams(() => {
+      searchParams.set('id', id);
+      return searchParams;
+    });
+  };
+
   return (
     <div className="flex gap-x-6">
-      <div className="grid w-fit grid-cols-5 gap-2">
+      <div className="grid h-fit w-fit grid-cols-5 gap-2">
         {data?.map(({ id, image, name }) => (
           <div
             key={id}
             className="group relative h-[198px] w-[112px] cursor-pointer rounded-2xl bg-cover bg-center"
             style={{ backgroundImage: `url(${image})` }}
-            onClick={() => onSelectAvatar(id)}
+            onClick={() => handleSelect(id)}
           >
             <div className="hidden h-full w-full items-end rounded-2xl bg-[linear-gradient(179.79deg,rgba(0,0,0,0)_61.4%,#000_99.82%)] px-2 pb-2 group-hover:flex">
               <p className="text-base leading-[1.2] text-white">{name}</p>
@@ -44,11 +50,6 @@ export function AvatarsList() {
       <AvatarPreview id={id} />
     </div>
   );
-
-  function onSelectAvatar(avatarId: string) {
-    searchParams.set('id', avatarId);
-    navigate(`?${searchParams.toString()}`);
-  }
 }
 
 const skeletonMock = Array.from({ length: 10 }, (_, i) => i);
